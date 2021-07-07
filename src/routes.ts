a2c1828264e5493c3718b11335c69dd8e6c0a864
andremoresco <amoresco1@gmail.com>
2021-07-07 11:16:48 -0300

    Add a jwt check

--- src/routes.ts
+++ src/routes.ts
@@ -4,14 +4,30 @@ import { json } from 'body-parser'
 import { listUserController } from './useCases/users/listUsers';
 import { listBudgetsController } from './useCases/budgets/listBudgets';
 import { createBudgetController } from './useCases/budgets/createBudget';
+import jwt from 'express-jwt';
+import jwks from 'jwks-rsa';
+
+const jwtCheck = jwt({
+    secret: jwks.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: 'https://motifi.us.auth0.com/.well-known/jwks.json'
+    }),
+    audience: 'http://localhost:8180',
+    issuer: 'https://motifi.us.auth0.com/',
+    algorithms: ['RS256'],
+    requestProperty: 'auth'
+})
 
 const router = Router();
 
-router.post('/users', json(), (request, response) => {
+router.post('/users', jwtCheck, json(), (request, response) => {
     return createUserController.handle(request, response);
 });
 
-router.get('/users', (request, response) => {
+router.get('/users', jwtCheck, (request, response) => {
+    console.log(request);
     return listUserController.handle(request, response);
 });
 
